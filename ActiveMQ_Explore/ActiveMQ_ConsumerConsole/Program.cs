ad0ccbe88874686a0b1ac5c3af933565717a0ad8
bjoseph <bjoseph@cyrotek.org>
2018-03-03 20:14:30 -0500

    Created Sample console applications to explore working with ActiveMQ server hosted in AWS-EC2.

---
+++ ActiveMQ_Explore/ActiveMQ_ConsumerConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveMQ_ConsumerConsole
+{
+    class Program
+    {
+        public const string URI = "activemq:tcp://34.239.133.119:61616";
+        public const string DESTINATION = "queue://aws.message.relay";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IConnectionFactory connectionFactory = new ConnectionFactory(URI);
+                IConnection _connection = connectionFactory.CreateConnection();
+                _connection.Start();
+                ISession _session = _connection.CreateSession();
+                IDestination dest = _session.GetDestination(DESTINATION);
+                using (IMessageConsumer consumer = _session.CreateConsumer(dest))
+                {
+                    Console.WriteLine("Listener started.");
+                    Console.WriteLine("Listener created.rn");
+                    IMessage message;
+                    while (true)
+                    {
+                        message = consumer.Receive();
+                        if (message != null)
+                        {
+                            ITextMessage textMessage = message as ITextMessage;
+                            if (!string.IsNullOrEmpty(textMessage.Text))
+                            {
+                                //Chat pMesg = JsonConvert.DeserializeObject<Chat>(textMessage.Text);
+
+                                Console.WriteLine($"Message Received: {textMessage.Text}");
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Press <ENTER> to exit.");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ ActiveMQ_Explore/ActiveMQ_ProducerConsole/Program.cs
@@ -0,0 +1,80 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveMQ_ProducerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var producer = new Publisher();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a message to send to the client:\n");
+
+                var messageToSend = Console.ReadLine();
+
+                var result = producer.SendMessage(messageToSend);
+
+                Console.WriteLine(result);
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class BaseClass
+    {
+        public const string URI = "activemq:tcp://34.239.133.119:61616";
+        public IConnectionFactory connectionFactory;
+        public IConnection _connection;
+        public ISession _session;
+
+        public BaseClass()
+        {
+            connectionFactory = new ConnectionFactory(URI);
+            if (_connection == null)
+            {
+                _connection = connectionFactory.CreateConnection();
+                _connection.Start();
+                _session = _connection.CreateSession();
+            }
+        }
+    }
+
+    public class Publisher : BaseClass
+    {
+        public const string DESTINATION = "queue://aws.message.relay";
+
+        public Publisher()
+        {
+        }
+
+        public string SendMessage(string message)
+        {
+            string result = string.Empty;
+            try
+            {
+                IDestination destination = _session.GetDestination(DESTINATION);
+                using (IMessageProducer producer = _session.CreateProducer(destination))
+                {
+                    var textMessage = producer.CreateTextMessage(message);
+                    producer.Send(textMessage);
+                }
+                result = "Message sent successfully.";
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+    }
+}
